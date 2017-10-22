430eb09707ab5a01ef1fadc5b8d4b57121144ddc
ZacharyCauchi <thatzacharycauchi@gmail.com>
2017-10-22 23:25:09 +1000

    bug fixes and style changes

--- view/navbarAnonymous.php
+++ view/navbarAnonymous.php
@@ -3,14 +3,14 @@
         <h1 onClick='scrollFunction3()'>Pantry Pal</h1>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-            <li class="navItem" onClick='scrollFunction1()'>Recipes</li>
-            <li class="navItem" onClick='scrollFunction2()'>About</li>
+            <li onClick='scrollFunction1()'><a><div class="navItem">Recipes</div></a></li>
+            <li onClick='scrollFunction2()'><a><div class="navItem">About</div></a></li>
             <li><a href="#loginModal" class="modal-trigger"><div class="navItem">Login</div></a></li>
         </ul>
         <ul class="side-nav" id="mobile-demo">
-            <li class="navItem" onClick='scrollFunction1()'>Recipes</li>
-            <li class="navItem" onClick='scrollFunction2()'>About</li>
-            <li class="navItem" onClick="loginModalOpen()">Login</li>
+            <li onClick='scrollFunction1()'><a><div class="navItem">Recipes</div></a></li>
+            <li onClick='scrollFunction2()'><a><div class="navItem">About</div></a></li>
+            <li><a href="#loginModal" class="modal-trigger"><div class="navItem">Login</div></a></li>
         </ul>               
     </div>
 </nav>
--- view/navbarUser.php
+++ view/navbarUser.php
@@ -3,16 +3,16 @@
         <h1 onClick='scrollFunction3()'>Pantry Pal</h1>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-            <li class="navItem" onClick='scrollFunction1()'>Recipes</li>
-            <li class="navItem" onClick='scrollFunction2()'>About</li>
-            <li class="navItem" onClick="showAccountUpdate(<?php echo $_SESSION['userID']; ?>)">Me</li>
+            <li onClick='scrollFunction1()'><a><div class="navItem">Recipes</div></a></li>
+            <li onClick='scrollFunction2()'><a><div class="navItem">About</div></a></li>
+            <li onClick="showAccountUpdate(<?php echo $_SESSION['userID']; ?>)"><a><div class="navItem">Me</div></a></li>
             <li><a href="loginController.php?logout=true"><div class="navItem">Logout</div></a></li>
         </ul>
         <ul class="side-nav" id="mobile-demo">
-            <li class="navItem" onClick='scrollFunction1()'>Recipes</li>
-            <li class="navItem" onClick='scrollFunction2()'>About</li>
-            <li class="navItem" onClick="showAccountUpdate(<?php echo $_SESSION['userID']; ?>)">Me</li>
-            <li><a href="loginController.php?logout=true"><div class="navItem">Logout</div></a></li>
+        <li onClick='scrollFunction1()'><a><div class="navItem">Recipes</div></a></li>
+        <li onClick='scrollFunction2()'><a><div class="navItem">About</div></a></li>
+        <li onClick="showAccountUpdate(<?php echo $_SESSION['userID']; ?>)"><a><div class="navItem">Me</div></a></li>
+        <li><a href="loginController.php?logout=true"><div class="navItem">Logout</div></a></li>
         </ul>               
     </div>
 </nav>
