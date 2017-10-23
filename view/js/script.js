876b14455d94f2f3f6e5888421c04d716c525692
ZacharyCauchi <thatzacharycauchi@gmail.com>
2017-10-23 16:45:45 +1000

    svg like buttons added

--- view/js/script.js
+++ view/js/script.js
@@ -104,19 +104,21 @@ function recipeSearch(ingArr){
         recipeSec.innerHTML = '';
         for(i = 0; i < recipeList.length; i++){
             var n = document.createElement("div");
+            var n2 = document.createElement("div");
             var t = document.createTextNode(recipeList[i].title);
-            n.appendChild(t);
+            n2.setAttribute("class", "recipeTitle");
+            n2.appendChild(t);
+            n.appendChild(n2)
             n.className = "recipeStyle card";
             n.setAttribute("onClick", "getRecipeInfo(" + recipeList[i].id + ")");
             recipeSec.appendChild(n);
             var a = document.createElement("a");
             a.setAttribute("class", "btn-floating btn-large waves-effect waves-light red likeIcon");
-            var i = document.createElement("i");
-            i.setAttribute("class", "material-icons likeButton");
-            i.innerHTML = '<svg class="heart" x="0px" y="0px" viewBox="0 0 100 125" xml:space="preserve"><path d="M35.7,19.7c-5.2,0-10.4,2-14.3,5.9c-7.9,7.9-7.9,20.8,0,28.7l27,25.4c0.5,0.4,1.1,0.7,1.7,0.7  c0.6,0,1.2-0.2,1.7-0.7l27-25.4c3.9-3.9,5.9-9.1,5.9-14.3c0-5.2-2-10.4-5.9-14.3c-3.5-3.5-8-5.1-12.7-5.1c-5.7,0-11.5,2.4-15.8,6.6  c0,0-0.1,0.1-0.1,0.1c0,0-0.1,0-0.1-0.1C45.9,23.4,40.8,19.7,35.7,19.7L35.7,19.7z"/>'
-            a.appendChild(i);
+            var icon = document.createElement("img");
+            icon.setAttribute("class", "likeButton");
+            icon.setAttribute("src", "../view/images/heartIcon.svg");
+            a.appendChild(icon);
             n.appendChild(a);
-
         }
     })
     .fail(function(msg){
